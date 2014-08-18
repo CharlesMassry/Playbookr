fa67705c79385b4941fbcff16b82dfc8caad9b8c
Charles Massry <charlesdmassry@gmail.com>
2014-08-18 13:59:52 -0400

    clean up tests

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "As a user" do
   scenario "I can sign up for playbookr" do
-    @user = FactoryGirl.build(:coach)
+    user = FactoryGirl.build(:coach)
 
     visit root_path
 
@@ -10,17 +10,17 @@ feature "As a user" do
 
     expect(page).to have_selector("form")
 
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password_digest
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password_digest
     click_button "Sign up"
 
-    expect(page).to have_content(@user.email)
-    expect(User.find_by(email: @user.email).email).
-      to have_content(@user.email)
+    expect(page).to have_content(user.email)
+    expect(User.find_by(email: user.email).email).
+      to eq(user.email)
   end
 
   scenario "I can sign in to playbookr" do
-    @user = FactoryGirl.create(:coach)
+    user = FactoryGirl.create(:coach)
 
     visit root_path
 
@@ -28,12 +28,12 @@ feature "As a user" do
 
     expect(page).to have_selector("form")
 
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password_digest
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password_digest
     click_button "Sign in"
 
-    expect(page).to have_content(@user.email)
-    expect(User.find_by(email: @user.email).email).
-      to have_content(@user.email)
+    expect(page).to have_content(user.email)
+    expect(User.find_by(email: user.email).email).
+      to have_content(user.email)
   end
 end
