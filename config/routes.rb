8a3508f89667942291c60d19c9afd90fe036e02a
Charles Massry <charlesdmassry@gmail.com>
2014-08-18 16:06:29 -0400

    User can create Team
    * A signed in user with no team can create one

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "landings#show"
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :teams, only: [:new, :create, :show]
 end
---
+++ spec/features/team_features_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+feature "Team creation" do
+  scenario "I can create a team if I don't have one" do
+    team = FactoryGirl.build(:team)
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+
+    visit root_path
+
+    click_link "Create team"
+
+    fill_in "Name", with: team.name
+
+    click_button "Create Team"
+
+    expect(page).to have_content(team.name)
+  end
+
+  scenario "I cannot create a team if I have one" do
+    team = FactoryGirl.create(:team)
+    user = FactoryGirl.create(:user)
+    user.team = team
+    sign_in(user)
+
+    visit root_path
+
+    expect(page).to_not have_link("Create team")
+  end
+end
