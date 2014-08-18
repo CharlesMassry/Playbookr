0da3aad1480f4c16330f32fdc2409d3e0ad9edf5
Charles Massry <charlesdmassry@gmail.com>
2014-08-18 16:06:29 -0400

    User can create Team
    * A signed in user with no team can create one

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,31 @@
+class TeamsController < ApplicationController
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      current_user.update(team: @team)
+      redirect_to @team
+    else
+      flash[:error] = "Invalid Team"
+      render :new
+    end
+  end
+
+  def show
+    @team = find_team
+  end
+
+  private
+
+  def find_team
+    Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818152612) do
+ActiveRecord::Schema.define(version: 20140818193636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,13 +32,21 @@ ActiveRecord::Schema.define(version: 20140818152612) do
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "team_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["team_id"], name: "index_users_on_team_id", using: :btree
 
 end
