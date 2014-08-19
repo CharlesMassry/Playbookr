ac9e313dfad18e4ce8383763c0b5634137356138
Charles Massry <charlesdmassry@gmail.com>
2014-08-18 20:20:45 -0400

    fix whitespace

--- spec/features/team_features_spec.rb
+++ spec/features/team_features_spec.rb
@@ -4,8 +4,8 @@ feature "Team creation" do
   scenario "I can create a team if I don't have one" do
     team = FactoryGirl.build(:team)
     user = FactoryGirl.create(:user)
-    sign_in(user)
 
+    sign_in(user)
     visit root_path
     click_link "Create team"
     fill_in "Name", with: team.name
@@ -17,8 +17,8 @@ feature "Team creation" do
   scenario "I cannot create a team if I have one" do
     team = FactoryGirl.create(:team)
     user = FactoryGirl.create(:user, team: team)
-    sign_in(user)
 
+    sign_in(user)
     visit root_path
 
     expect(page).not_to have_link("Create team")
