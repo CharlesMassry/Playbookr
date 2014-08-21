8465d355652a27b5e9263ffb0f1b653e574aede7
Charles Massry <charlesdmassry@gmail.com>
2014-08-21 12:55:47 -0400

    clean up migration file

--- db/migrate/20140819170252_add_not_null_to_media_play_id.rb
+++ db/migrate/20140819170252_add_not_null_to_media_play_id.rb
@@ -1,5 +1,5 @@
 class AddNotNullToMediaPlayId < ActiveRecord::Migration
   def change
-    change_column_null :media, :play_id, null: false
+    change_column_null :media, :play_id, false
   end
 end
