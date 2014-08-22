0f8ab0ea6e0ef175ef572bad58fbd0bc07cf3f96
Charles Massry <charlesdmassry@gmail.com>
2014-08-22 11:21:49 -0400

    soothe the hound

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( video-js.swf vjs.eot vjs.woff vjs.ttf )
+Rails.application.config.assets.precompile += %w(
+  video-js.swf
+  vjs.eot
+  vjs.woff
+  vjs.ttf)
