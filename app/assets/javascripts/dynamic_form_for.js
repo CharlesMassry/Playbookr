650a49ae20e18149f3745ec74cd89f32463f3453
Charles Massry <charlesdmassry@gmail.com>
2014-08-27 11:35:35 -0400

    refactor javascript

--- app/assets/javascripts/dynamic_form_for.js
+++ app/assets/javascripts/dynamic_form_for.js
@@ -4,8 +4,8 @@ function capitalize(string) {
 
 function dynamicFormFor() {
   var button = $(this);
-  var type = button.attr("data").replace(/_\d*/,"");
-  var commentableId = button.attr("data").replace(/\D*/, "");
+  var type = button.data("type");
+  var commentableId = button.data("id");
   var form = $("<form id='" + type + "_comment_form_" + commentableId + "'>");
   var input = $("<textarea name='comment[body]' id='input_" + type + "_comment' type='text'>");
   var commentableTypeField = $("<input type='hidden' name='comment[commentable_type]' id='commentable_type' value='"+ capitalize(type) +"'>");
