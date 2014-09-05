45dbe6ec70d6d334ea64bc07213ddf554a4556aa
Charles Massry <charlesdmassry@gmail.com>
2014-09-05 17:26:30 -0400

    abstract out environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
-    domain:               "playbookr.us",
+    domain:               ENV["DOMAIN"],
     user_name:            "noreply@playbookr.us",
     password:             ENV["PASSWORD"],
     authentication:       "plain",
