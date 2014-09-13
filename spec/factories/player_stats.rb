f5bf2405e5643e2c19100eac47903b62eb31274f
Charles Massry <charlesdmassry@gmail.com>
2014-09-13 13:04:27 -0400

    make tests pass

--- spec/factories/player_stats.rb
+++ spec/factories/player_stats.rb
@@ -1,9 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :player_stat do
     association :team
-    association :player
-    stats ""
+    association :player, factory: :user
+    stats '{ "HR": 24, "BA": 0.3 }'
   end
 end
