033fec784b28e5b0ce92c56cdc6991cd5d2b3099
Charles Massry <charlesdmassry@gmail.com>
2014-09-14 14:59:07 -0400

    fix bug and whitespace

--- app/controllers/player_stats_controller.rb
+++ app/controllers/player_stats_controller.rb
@@ -1,6 +1,6 @@
 class PlayerStatsController < ApplicationController
   def show
-    @player_stat_column = current_user.team.player_stat_column
+    @player_stat_column = current_user.team.player_stat_columns
     @players = current_user.team.players
   end
 end
--- spec/factories/team_stat_columns.rb
+++ spec/factories/team_stat_columns.rb
@@ -1,5 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :team_stat_column do
     column_names ["HR", "BA"]
