023066e2275c9c238ee98c1819b367b8cffc813b
Charles Massry <charlesdmassry@gmail.com>
2014-09-15 11:35:12 -0400

    fix bug in view

--- app/assets/javascripts/angular/controllers/player_stats_controller.js
+++ app/assets/javascripts/angular/controllers/player_stats_controller.js
@@ -1,4 +1,4 @@
-playerStats.controller("playerStatsController", ["$scope", "$location", "$http",function($scope, $location, $http){
+playerStats.controller("playerStatsController", ["$scope", "$http",function($scope, $http){
   var path = location.pathname + ".json";
   $http.get(path).success(function(data){
     $scope.columns = data.player_stat_column;
